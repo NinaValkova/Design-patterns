dd38b3154c57e464c6bffedb8be8107d5316d6a4
NinaValkova <nina.p.valkova@gmail.com>
2025-11-27 02:55:05 +0200

    Added decorator design pattern

---
+++ Decorator/decorator_coding_exercise.py
@@ -0,0 +1,40 @@
+"""
+You are given two types, Circle and Square, and a decorator called ColoredShape.
+
+The decorator adds the color to the string output for a given shape, just as we did in the lecture.
+
+There's a trick though: the decorator now has a resize() method that should resize the underlying shape. However, only the Circle has a resize() method; the Square does not — do not add it!
+
+You are asked to complete the implementation of Circle, Square and ColoredShape.
+"""
+
+class Circle:
+  def __init__(self, radius):
+    self.radius = radius
+
+  def resize(self, factor):
+    self.radius *= factor
+
+  def __str__(self):
+    return f'A circle of radius {self.radius}'
+
+class Square:
+    def __init__(self, side):
+        self.side = side
+
+    def __str__(self):
+        return f"A square with side {self.side}"
+
+
+class ColoredShape:
+  def __init__(self, shape, color):
+    self.color = color
+    self.shape = shape
+
+  def resize(self, factor):
+    if isinstance(self.shape, Circle):
+        self.shape.resize(factor)
+    # note that a Square doesn't have resize()
+
+  def __str__(self):
+    return f'{self.shape} has the color {self.color}'
